3c71e64ebeb5c883d77b7d71ed79277ec4cffdcb
Jakub Was <wasjakub.jw@gmail.com>
2021-12-11 15:19:01 +0100

    refactor: :recycle: Added webpack-loader config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import "./styles/styles.css";
+import IMAGE from "./assets/webpack-logo.png";
+import LOGO from "./assets/webpack-logo.svg";
+
 const App = () => {
-  return <h1>Hello from App</h1>;
+  return (
+    <>
+      <h1>Hello from App</h1>
+      <img src={IMAGE} alt="Webpack Image PNG" width="200px" />
+      <img src={LOGO} alt="Webpack Image SVG" width="200px" />
+    </>
+  );
 };
 
 export default App;

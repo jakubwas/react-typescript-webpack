14a2ce357caa6aec677d3c6646448fbe7b689d0f
Jakub Was <wasjakub.jw@gmail.com>
2021-12-11 15:45:43 +0100

    refactor: :recycle: Added webpack configs for different modes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import LOGO from "./assets/webpack-logo.svg";
 const App = () => {
   return (
     <>
-      <h1>Hello from App</h1>
+      <h1>
+        React + TypeScript + Webpack - running in {process.env.NODE_ENV},{" "}
+        {process.env.name}
+      </h1>
       <img src={IMAGE} alt="Webpack Image PNG" width="200px" />
       <img src={LOGO} alt="Webpack Image SVG" width="200px" />
     </>
